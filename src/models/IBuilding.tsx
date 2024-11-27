11a70f9333734452e2d3aa8606492b6e8526656b
Lucy Hühn <666nikkineko@gmail.com>
2024-11-27 23:10:36 +0100

    backup commit

--- src/models/IBuilding.tsx
+++ src/models/IBuilding.tsx
@@ -1,11 +1,12 @@
 import IRessource from "./IRessource";
+import User from "./User";
 
 interface IBuilding {
   state: boolean;
   desc: string;
   switchState: () => void; // TODO implement boolean
   setDesc: (newDesc: string) => void; // TODO implement boolean
-  production: () => IRessource;
+  production: (user: User) => IRessource;
 }
 
 export default IBuilding;
